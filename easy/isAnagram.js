8c997f7cde9884a2ea75085e49c4cd42aebc3226
ybleeho <hu.li@6e-group.com>
2019-03-28 22:48:32 +0800

    isAnagram

---
+++ easy/isAnagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    s = s.split('').sort().join('');
+    t = t.split('').sort().join('');
+    if(s === t)
+        return true
+    return false
+};
+
+console.log(isAnagram('abc','cab'))
